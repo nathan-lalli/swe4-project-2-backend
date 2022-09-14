34a70bb1f8c24e3874bced715c0e9e97079ae379
Nathan Lalli <nathan.lalli@eagles.oc.edu>
2022-09-14 10:41:58 -0500

    fix cors policy

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ var cors = require("cors");
 var app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3003",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
+  origin: "*",
 };
 
 // view engine setup
